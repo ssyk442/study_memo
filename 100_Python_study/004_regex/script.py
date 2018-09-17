6b6c026faa5e8c1939b2bf3e0e11b69f589d3e81
ssyk442 <38380803+ssyk442@users.noreply.github.com>
2018-09-17 22:43:33 +0900

    Delete script.py

--- 100_Python_study/004_regex/script.py
+++
@@ -1,5 +0,0 @@
-from searchMobileNumber import SearchMobileNumber
-
-text = \
- input('Please write or paste the message \nwhich might include mobile numbers.: ')
-print(SearchMobileNumber(text).is_mobile_number())
